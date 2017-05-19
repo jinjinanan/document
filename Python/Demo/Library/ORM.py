6a305583e943579293784685987cb765830391ad
陈林波 <jinjinanan@hotmail.com>
2017-05-19 11:03:45 +0800

    Python添加pymysql ，简单的增删查。 初步添加了ORM

---
+++ Python/Demo/Library/ORM.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#著名的Python ORM 1.SQLAlchemy, 2.SQLObject
+
+import SQLAlchemy
+
--- Python/Demo/Library/StudyPyMysql.py
+++ Python/Demo/Library/StudyPyMysql.py
@@ -2,28 +2,66 @@
 # -*- coding: utf-8 -*-
 #http://www.cnblogs.com/wt11/p/6141225.html
 import pymysql
+from pymysql import OperationalError
 
 #创建连接
-conn = pymysql.connect(host = '120.25.247.186',
-                       port= 3306,
-                       user = 'clbMacBookPro',
-                       passwd = 'clbMacBookPro',
-                       db = 'awesome',
-                       charset = 'utf-8'
-                       )
-#创建游标
-cursor = conn.cursor()
+def connect():
+    try:
+        conn = pymysql.connect(host='120.25.247.186',
+                               port=3306,
+                               user='clbMacBookPro',
+                               password='clbMacBookPro',
+                               database='awesome',
+                               charset='utf8'  # 只需要utf8
+                               )
+    except OperationalError as e :
+        print('连接数据库错误')
+        return None
 
-#执行SQL 并返回影像数据
-# effect_row = cursor.execute('SELECT * FROM wordList')
-effect_row = cursor.execute('INSERT INTO wordList VALUES(once,一旦；曾经，wʌns，)')
+    # 创建游标
+    cursor = conn.cursor()
+    return cursor,conn
 
-#提交，不然无法保存新建或者修改的数据
-conn.commit()
+def insert(cur,word,chinese,aPhoneticSymbol = '',note = ''):
+    sql = ' INSERT INTO wordList (word,chinese,aPhoneticSymbol,note) ' \
+          'VALUES("%s","%s","%s","%s");' % (word,chinese,aPhoneticSymbol,note)
+    effect_row = cur.execute(
+        sql)
+    print('执行sql语句' + sql, cur.lastrowid)
+    return cur
 
-#关闭游标
-cursor.close()
+def query(cur):
+    cur.execute('SELECT * FROM wordList')
+    row = cur.fetchall()
+    print(row)
+    return cur
 
-#关闭连接
-conn.close()
+def delect(cur,word):
+    sql = 'DELETE FROM wordList WHERE word = \'%s\'; ' % (word)
+    print('执行删除语句：'+ sql)
+    effect_row = cur.execute(sql)
+    print('DELECT' + str(effect_row) )
+    return cur
+
+
+def main():
+    cursor,conn = connect()
+    if cursor :
+        print('进入')
+        insert(cursor,'once','一旦；曾经','wʌns','')
+        query(cursor)
+        delect(cursor,'once')
+    # 提交，不然无法保存新建或者修改的数据
+    conn.commit()
+
+    # 关闭游标
+    cursor.close()
+
+    # 关闭连接
+    conn.close()
+
+main()
+
+
+
 
