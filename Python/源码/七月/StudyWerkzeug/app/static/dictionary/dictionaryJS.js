fc731bc944087eaf17a2bcf6b8410223e85f84f0
陈林波 <jinjinanan@hotmail.com>
2017-08-21 10:44:14 +0800

    添加焦点推荐导航

--- Python/源码/七月/StudyWerkzeug/app/static/dictionary/dictionaryJS.js
+++ Python/源码/七月/StudyWerkzeug/app/static/dictionary/dictionaryJS.js
@@ -17,3 +17,74 @@ $(document).ready(function(){
 });
 
 
+function backAndforth() {
+
+    var o = $(".xm-carousel-list").css("margin-left");
+
+    if(o == "0px")
+    {
+        $(".xm-carousel-list").css("margin-left","-1226px");
+        // console.log(o);
+    }
+    else
+    {
+                $(".xm-carousel-list").css("margin-left","0px");
+
+
+    }
+
+        // console.log("阿" ,o);
+
+
+};
+
+$(document).ready(function () {
+    setInterval(backAndforth,1000 *2);
+});
+
+function controlHomeSlider() {
+    var mylist = $(".xm-slider").children();
+    for(var i = 0 ;i < mylist.length; i++ )
+    {
+        var value = "float: none; position: absolute; display: block;";
+        var value2 = "float: none; position: absolute;";
+        var value1 = "float: none; position: absolute; display: none;";
+        var a = mylist[i];
+        var $a = $(a);
+
+
+
+        // console.log($a.attr("style"),$a);
+        if($a.attr("style") == value || $a.attr("style") == value2)
+        {
+            // $a.attr(;
+            $a.fadeToggle();
+
+            // $a.attr("style",value1);
+            var $b =  $a.next();
+            if(i < (mylist.length - 1))
+            {
+
+                $b.fadeToggle();
+                // $b.attr("style",value)
+            }
+            else
+            {
+
+                $(mylist[0]).fadeToggle();
+                // $(mylist[0]).attr("style",value)
+            }
+
+           break;
+        }
+
+    }
+    console.log(mylist);
+
+}
+
+$(document).ready(function () {
+    setInterval(controlHomeSlider,1000*3)
+    // controlHomeSlider();
+
+})
